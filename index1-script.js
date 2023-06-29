79e5626c6de019b55fdf204b0ee6056720401f17
llcain <llcain2012@gmail.com>
2023-06-28 21:48:34 -0500

    Updated index1 file

--- index1-script.js
+++ index1-script.js
@@ -1,6 +1,13 @@
-const element = document.getElementById("myBtn");
-element.addEventListener("click", myFunction);
+// const element = document.getElementById("myBtn");
+// element.addEventListener("click", myFunction);
+
+// function myFunction() {
+//   document.getElementById("demo").innerHTML = "Hello World";
+// }
+
+const element = document.getElementById('myBtn');
+element.addEventListener('click', myFunction);
 
 function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello World";
+  document.getElementById('demo').innerHTML = "Chicken";
 }
